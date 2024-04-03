9705cae9e38ba07d0a2e96110dd4ed14f86f9fb3
Kama <nanyi14815@gmail.com>
2024-04-04 00:07:56 +0800

    [Updated] 1. ico 與 title 變更 2. 完成左側開合狀態

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
+
+// Component
 import LeftSideBar from "./components/LeftSideBar";
 
 const App = () => {
-	const [toggle, setToggle] = useState(false);
-
-	const handdleToggle = () => {
-		setToggle(!toggle);
+	const leftSideBarRef = useRef(null);
+	// LeftSideBar 元件的狀態和函數
+	const ToggleStatus = {
+		CLOSED: 0, // 關閉
+		LIST: 1, // 清單功能
+		CHART: 2, // 分析圖表
 	};
+	// 表示當前開合狀態。
+	const [toggle, setToggle] = useState(ToggleStatus.CLOSED);
+	const handdleToggle = (mode) => {
+		// 如果當前的狀態已經是要切換的狀態，則設置狀態為關閉
+		// 否則設置狀態為要切換的狀態
+		setToggle((prevToggle) => (prevToggle === mode ? ToggleStatus.CLOSED : mode));
+	};
+
+	useEffect(() => {
+		const handleOutsideClick = (event) => {
+			// 檢查點擊事件的目標是否在 LeftSideBar 之外
+			if (leftSideBarRef.current && !leftSideBarRef.current.contains(event.target)) {
+				handdleToggle(ToggleStatus.CLOSED);
+			}
+		};
+
+		document.addEventListener("mousedown", handleOutsideClick);
+
+		return () => {
+			document.removeEventListener("mousedown", handleOutsideClick);
+		};
+	}, []);
 
 	return (
 		<div className="relative h-screen flex flex-row justify-start">
 			{/* Left SideBar */}
-			<LeftSideBar />
+			<div ref={leftSideBarRef}>
+				<LeftSideBar toggle={toggle} handdleToggle={handdleToggle} ToggleStatus={ToggleStatus} />
+			</div>
 			{/* Main Content */}
 			<div>123456</div>
 		</div>
--- src/components/LeftSideBar.jsx
+++ src/components/LeftSideBar.jsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-const LeftSideBar = () => {
-	const [toggle, setToggle] = React.useState(false);
+// Icon
+import listIcon from "../assets/icons/icon-list.svg";
+import analysisIcon from "../assets/icons/icon-analysis.svg";
 
-	const handdleToggle = () => {
-		setToggle(!toggle);
-	};
-
+/**
+ * 一個用於左側功能選單的 React 組件
+ * @component
+ * @param {number} toggle - 當前的開合狀態
+ * @param {function} handdleToggle - 切換狀態的函數
+ * @param {Object} ToggleStatus - 狀態的枚舉物件
+ * @returns {ReactElement} 返回一個 React 元素
+ */
+const LeftSideBar = ({ toggle, handdleToggle, ToggleStatus }) => {
 	return (
 		<div
-			className={`h-screen bg-light flex items-start ${
-				// toggle ? "w-20" : "w-1/2"
-				"w-1/2"
+			className={`relative h-screen bg-light flex items-start ${
+				toggle === ToggleStatus.CLOSED ? "md:w-20 w-10" : "md:w-[50vw] w-[100vw]"
 			} duration-300 shadow-[0_0_40px_#0000003D]`}>
-			<div className="flex w-20 h-full relative bg-customText"></div>
+			<div className="flex flex-col items-center justify-center md:w-20 w-10 h-full relative bg-customText md:gap-0 gap-3">
+				<button
+					type="button"
+					className="inline-flex justify-center items-center md:w-20 w-10 aspect-square"
+					onClick={() => {
+						handdleToggle(ToggleStatus.LIST);
+					}}>
+					<img src={listIcon} className="md:w-fit w-full p-1 aspect-square" alt={"Analysis Icon"} />
+				</button>
+				<button
+					type="button"
+					className="inline-flex justify-center items-center md:w-20 w-10 aspect-square"
+					onClick={() => {
+						handdleToggle(ToggleStatus.CHART);
+					}}>
+					<img src={analysisIcon} className="md:w-fit w-full p-1 aspect-square" alt={"Analysis Icon"} />
+				</button>
+			</div>
 		</div>
 	);
 };
