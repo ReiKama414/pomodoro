14ab222bdd67cff58057a0f01f8105d98f25c396
Kama <milu77704@gmail.com>
2024-04-03 15:46:20 +0800

    [Updated] Create Project

---
+++ src/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import App from "./App";
+
+// Style
+import "./index.sass";
+
+// Router
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		errorElement: <>ErrorMessage</>,
+	},
+	{
+		element: <>404</>,
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
+
+reportWebVitals();
